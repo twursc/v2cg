29289938f8b650dc19df70ae0038a3ce87d1aa36
twursc <msysgit@lenovo-t470p>
2018-08-21 16:48:47 +0800

    Add favicon

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <title>V2CG</title>
     <link href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
     <link href="./resources/stylesheet.css" rel="stylesheet">
+    <link rel="shortcut icon" type="image/png" href="./resources/favicon.ico" id="favicon">
     <!--[if lt IE 9]>
         <script src="https://cdn.bootcss.com/html5shiv/3.7.3/html5shiv.min.js"></script>
         <script src="https://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
