6d2bd1788468ec33f28804039967d2a1871f33aa
twursc <msysgit@lenovo-t470p>
2018-09-26 19:59:33 +0800

    Project save

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="zh-CN">
+<html lang="zh-CN" class="theme-light">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -15,9 +15,6 @@
 </head>
 
 <body>
-
-
-
     <div class="container" style="padding:15px;">
         <div>
             <!-- Nav tabs -->
@@ -27,7 +24,7 @@
                 <li role="presentation"><a href="#inbounds" aria-controls="home" role="tab" data-toggle="tab">Inbounds</a></li>
                 <li role="presentation"><a href="#outbounds" aria-controls="profile" role="tab" data-toggle="tab">Outbounds</a></li>
                 <li role="presentation"><a href="#routing" aria-controls="messages" role="tab" data-toggle="tab">Routing</a></li>
-                <li role="presentation"><a href="#transport" aria-controls="settings" role="tab" data-toggle="tab">Transports</a></li>
+                <li role="presentation"><a href="#transport" aria-controls="settings" role="tab" data-toggle="tab">Transports (Global)</a></li>
                 <li role="presentation"><a href="#policy" aria-controls="settings" role="tab" data-toggle="tab">Policy</a></li>
                 <li role="presentation"><a href="#api" aria-controls="settings" role="tab" data-toggle="tab">API</a></li>
                 <li role="presentation"><a href="#miscellaneous" aria-controls="settings" role="tab" data-toggle="tab">Miscellaneous</a></li>
@@ -37,10 +34,12 @@
             <!-- Tab panes -->
             <div class="tab-content">
                 <div role="tabpanel" class="tab-pane active" id="home">Home...</div>
+
                 <div role="tabpanel" class="tab-pane" id="inbounds">
                     <h3>Inbounds</h3>
                     <p class="panedesc">Configure inbound connections, see <a target="_blank" href="https://www.v2ray.com/chapter_02/02_protocols.html">Protocols</a></p>
                     <ul class="list-inline">
+
                         <li class="inline-item" id="inbounds-list">
                             <div class="conn-table-container">
                                 <table class="table table-hover">
@@ -56,12 +55,37 @@
                                 <button class="btn btn-default inlist-button"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></button>
                             </div>
                         </li>
-                        <li class="inline-item" id="inbound-config">
+
+                        <li class="inline-item" id="inbound-config" style="width: calc(100% - 260px);">
                             <div class="conn-table-container">
-                                <table>
-                                    <tr data-inbound-tag="to-japan" data-inbound-primary="1">
-                                        <td><img src="./resources/protocol-icons/vmess.png" style="width: 16px"></td><td>To Japan</td><td></td></tr>
-                                </table>
+                                <form class="form-horizontal">
+                                    <div class="form-group">
+                                        <label for="inboundTag" class="col-sm-2 control-label">Inbound Tag</label>
+                                        <div class="col-sm-10">
+                                            <input type="text" class="form-control" id="inboundTag" placeholder="">
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="form-group">
+                                        <label for="listenPort" class="col-sm-2 control-label">Port</label>
+                                        <div class="col-sm-10">
+                                            <input type="number" class="form-control" id="listenPort" placeholder="">
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="form-group">
+                                        <label for="protocol" class="col-sm-2 control-label">Protocol</label>
+                                        <div class="col-sm-10">
+                                            <select class="form-control" id="protocol">
+                                                <option value="shadowsocks">Shadowsocks</option>
+                                                <option value="socks5">Socks5</option>
+                                                <option value="http">HTTP</option>
+                                                <option value="vmess">VMess</option>
+                                                <option value="dokodemo-door">どこでもドア</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
                         </li>
                     </ul>
@@ -89,9 +113,23 @@
                 <div role="tabpanel" class="tab-pane" id="jsoncontent">
                     <h3>JSON Content</h3>
                     <p class="panedesc">Contents below are according to your configurations before, it can be used directly by V2Ray by pasting it into <code>config.json</code></p>
-                    <textarea class="jsonContent form-control" style="height: 540px;" onchange="jsonStorage.setItem('nz.v2cg.storage.content', this.value)">{}</textarea>
+                    <textarea class="jsonContent form-control" style="height: 540px; min-width: 100%; max-width: 100%;" onchange="jsonStorage.setItem('nz.v2cg.storage.content', this.value)">{}</textarea>
+                    <br><button class="btn btn-primary" onclick="parseJson($('textarea.jsonContent').val());">解析当前 JSON</button>
                 </div>
             </div>
+        </div>
+    </div>
+
+    <div class="proto_tmpl_container" style="display: none">
+        <div class="proto_tmpl" id="vmess">
+            <table class="table table-bordered">
+                <thead>
+                    <tr><td>Email</td><td>UUID</td><td>Level</td><td>Alter ID</td></tr>
+                </thead>
+                <tbody>
+                    <tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </body>
@@ -102,7 +140,23 @@
 <script src="./language/language_zh-CN.js"></script>
 <script src="./resources/config_handler.js"></script>
 <script>
+    content = [];
     jsonStorage = window.localStorage;
     $('textarea.jsonContent').val(jsonStorage.getItem("nz.v2cg.storage.content"));
+    window.onload = function() {
+        applyI18N();
+        parseJson($('textarea.jsonContent').val());
+    }
+    
+    function parseJson(content) {
+        try {
+            content = JSON.parse(content);
+            console.log(content);
+        } catch(e) {
+            alert(e);
+            console.error(e);
+        }
+        console.log("Inbound", content.inbound);
+    }
 </script>
 </html>
--- language/language_zh-CN.js
+++ language/language_zh-CN.js
@@ -3,11 +3,16 @@ i18N["zh-CN"] = {
     "Outbounds": "传出连接",
     "Routing": "路由",
     "Policy": "用户策略",
-    "Transports": "传输协议",
+    "Transports (Global)": "传输协议（全局）",
     "Miscellaneous": "杂项",
     "Logging": "日志",
     "Statistics": "流量统计",
     "JSON Content": "JSON 配置文件内容",
     "Contents below are according to your configurations before, it can be used directly by V2Ray by pasting it into <code>config.json</code>": "下面文本框中的内容是根据前面的配置实时更新的 JSON，将其填入 <code>config.json</code> 可直接用于 V2Ray。",
-    "Configure inbound connections, see <a target=\"_blank\" href=\"https://www.v2ray.com/chapter_02/02_protocols.html\">Protocols</a>": "配置 V2Ray 的传入连接，参见 <a target=\"_blank\" href=\"https://www.v2ray.com/chapter_02/02_protocols.html\">协议列表</a>"
+    "Configure inbound connections, see <a target=\"_blank\" href=\"https://www.v2ray.com/chapter_02/02_protocols.html\">Protocols</a>": "配置 V2Ray 的传入连接，参见 <a target=\"_blank\" href=\"https://www.v2ray.com/chapter_02/02_protocols.html\">协议列表</a>",
+    "Inbound Tag": "传入连接 标签",
+    "Port": "端口",
+    "Protocol": "协议类型",
+    "Shadowsocks": "纸飞机",
+    "どこでもドア": "任意门"
 };
